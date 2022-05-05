508fade170f504173209eae54d73b838356641f8
Liz Bringas <lizypizi@MacBook-Air-de-Liz.local>
2022-05-05 18:58:26 -0400

    Cleanning React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+       Titulo de la primera limpieza
+     </h1>
+    </div>
+  );
+}
+
+export default App;
