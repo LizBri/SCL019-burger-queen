d7a6c88983c51a577c8bfb00a9ad996cb9629e8e
Liz Bringas <lizypizi@MacBook-Air-de-Liz.local>
2022-05-18 23:12:26 -0400

    Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
 import './App.css';
+import React from 'react';
+import AppLogo from './components/AppLogo';
+import Mesas from './components/Mesas';
 
-function App() {
+
+ function App() {
+   const mesas = () =>{
+   return Mesas;
+   };
+
   return (
-    <div className="App">
-     <h1>
-       Titulo de la primera limpieza
-     </h1>
+    <div className="App mt-5">
+      <AppLogo/>
+      <button className='btn btn-warning' onClick={mesas}> Mesas
+        </button>
     </div>
   );
 }
---
+++ src/components/AppLogo.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment }from "react";
+import logo from "./img/logo.png";
+function AppLogo(){
+    return(
+        <Fragment>
+            <img src={logo} width="800px" alt="logo_home"/>
+        </Fragment>
+    )
+}
+
+export default AppLogo;
